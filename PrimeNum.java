ee4191c5dc852f9ffcb0ab034b4e20217f3e165a
bijay kumar swain <i.bijay@gmail.com>
2016-02-09 19:49:33 +0530

    Signed-off-by: bijay kumar swain <i.bijay@gmail.com>

--- PrimeNum.java
+++ PrimeNum.java
@@ -1,13 +1,13 @@
 public class PrimeNum{
 	public static void main (String[] args)
 	{
-		int i_max_limit_v = Integer.parseInt(args[0]), i, j;
+		int i_max_limit_v = Integer.parseInt(args[0]);
 		boolean b_prime_flg_v;
 		System.out.println("List of Prime Numbers upto" + i_max_limit_v);
-		for (i=2;i<=i_max_limit_v;i++)
+		for (int i=2;i<=i_max_limit_v;i++)
 		{
 			b_prime_flg_v = true;
-			for (j=2;j<=i;j++)
+			for (int j=2;j<=i;j++)
 			{
 				if (i%j==0 && j!=i)
 				{
